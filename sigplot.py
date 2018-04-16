86e77b903c185efd6483081a6e513d95b4f7a92d
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-04-16 16:30:00 +0900

    play

--- sigplot.py
+++ sigplot.py
@@ -1,13 +1,28 @@
 import wave
 import numpy as np
 import matplotlib.pyplot as plt
+import pyaudio
 
 def main():
-    wf = wave.open("2_1.wav", "r")
-    buf = wf.readframes(wf.getnframes())
-    data = np.frombuffer(buf, dtype="int16")
-    plt.plot(data)
-    plt.show()
+    wf = wave.open("2_1.wav", "rb")
+    p = pyaudio.PyAudio()
+
+    stream = p.open(format=p.get_format_from_width(wf.getsampwidth()),
+                    channels=wf.getnchannels(),
+                    rate=wf.getframerate(),
+                    output=True)
+
+    CHUNK = wf.getnframes()
+    data = wf.readframes(CHUNK)
+
+    while data != None:
+        stream.write(data)
+        data = wf.readframes(CHUNK)
+
+    stream.stop_stream()
+    stream.close()
+
+    p.terminate()
 
 if __name__ == "__main__":
     main()
