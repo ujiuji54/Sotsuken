c991b0380a34a1422679065ceb82b99ca3a3a2f6
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-07-09 14:26:53 +0900

    sweep

--- makedata/create.py
+++ makedata/create.py
@@ -7,7 +7,7 @@ import pyaudio
 def create_wave(A, f0, fs, t):#振幅、周波数、サンプリング周波数、秒数
 
     point = np.arange(0,fs*t)
-    sin_wave = A*np.sin(2*np.pi*f0*point/fs)
+    sin_wave = A*np.sin(2*np.pi*f0*(point/fs)*(point/fs))
 
     sin_wave = [int(x * 32767.0) for x in sin_wave]
     
--- makedata/main.py
+++ makedata/main.py
@@ -1,5 +1,5 @@
 from create import *
 from play import *
 
-create_wave(1,440,44100,1)
-play()
+create_wave(1,100,44100,100)
+#play()
