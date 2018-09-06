47542a5f5ff8ea09b4549ebdf4f24acdee2d651c
ujiuji54 <r114306@g.kobe-kosen.ac.jp>
2018-09-06 11:40:34 +0900

    sweep

--- makedata/create.py
+++ makedata/create.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import struct
 import pyaudio
 
-def create_wave(f0, fs):#振幅、周波数、サンプリング周波数
+def create_wave(f0, fs):#周波数、サンプリング周波数
 
     binwave = []
     #binwave = create_sin(1,f0,fs)
@@ -21,7 +21,7 @@ def create_wave(f0, fs):#振幅、周波数、サンプリング周波数
     print(len(binwave))
 
 def create_sin(A,f0,fs):#振幅、周波数、サンプリング周波数
-    point = np.arange(0,fs*20)
+    point = np.arange(0,fs*12.50) #等差数列
     sin_wave = A*np.sin(2*np.pi*f0*(point/fs)**3)
     sin_wave = [int(x * 32767.0) for x in sin_wave]
     return sin_wave
